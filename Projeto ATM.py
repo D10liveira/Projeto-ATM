ea04072e73bbadeac9d5f0a1e5a1e03507fad8b9
Dieimisson Oliveira <dieimisson.o@gmail.com>
2024-04-12 21:36:13 -0300

    Criação de novas funções

--- Projeto ATM.py
+++ Projeto ATM.py
@@ -2,137 +2,156 @@ class Banco:
     def __init__(self, conta_corrente, conta_universitaria):
         self.conta_corrente = conta_corrente
         self.conta_universitaria = conta_universitaria
-        self.saldo = 1000
+        self.saldo = 0
+        self.extrato = ""
     
-    def autentificacao(self):
+    def autenticacao(self):
         while True:
-            nome_completo = input("Por favor, insira seu nome completo: ")
-            CPF = input("Por favor, insira seu CPF: ")
+            print("\n================= BEM VINDO AO SEU CAIXA ELETRÔNICO =================\n")
+            nome_completo = input("Para continuar seu atendimento, insira seu nome completo: ").title()
             print()
-            if len(nome_completo.split()) >= 2 and len(CPF) == 11:
-                print("Bem-vindo,", nome_completo)
-                print()
-                return True
+            if len(nome_completo.split()) >= 2:
+                CPF = input(f"{nome_completo.split()[0]}, insira seu CPF: ")
+                if len(CPF) == 11:
+                    print("\nValidando seus dados...\n\n\n")
+                    print("Bem-vindo,", nome_completo, "\n")
+                    if self.selecionar_conta():
+                        self.acao()
+                    else:
+                        return False
+                else:
+                    print("\nO campo CPF precisa conter 11 dígitos.\nTente novamente.\n")
             else:
-                print("Dados incorretos. Tente novamente com nome e sobrenome.\nObs. Campo CPF precisa conter 11 dígitos.")
+                print("Erro: Insira seu NOME e SOBRENOME.\n")
             
-    def contas(self):
-        print("Selecione o tipo de conta:")
-        print("[1] Conta Corrente")
-        print("[2] Conta Universitária")
-        escolha = int(input())
+    def selecionar_conta(self):
+        print("Digite o número da opção desejada:\n")
+        print("[1] CONTA CORRENTE")
+        print("[2] CONTA UNIVERSITÁRIA")
+        print("[3] SAIR")
+        escolha = int(input("-- "))
         print()
-        
         if escolha == 1:
-            self.conta_corrente = True
-            self.conta_universitaria = False
+            return self.conta_corrente
         elif escolha == 2:
-            self.conta_corrente = False
-            self.conta_universitaria = True
+            return self.conta_universitaria
+        elif escolha == 3:
+            print("<-> Obrigado por usar nossos serviços! <->\n")
+            print("======== ATENDIMENTO ENCERRADO ========\n\n")
+            return False
         else:
-            print("Opção inválida!")
-            self.contas()
-        return True
+            print("Opção inválida!\n")
+            return self.selecionar_conta()
 
     def acao(self):
         if self.conta_corrente:
-            print("O que deseja fazer?")
-            print("[1] SAQUE")
-            print("[2] SALDO")
-            print("[3] DEPÓSITO")
-            opcao = int(input())
-            print()
-            
-            if opcao == 1:
-                valor = float(input("Informe a quantia para o saque: "))
+            print(">>>> CONTA CORRENTE <<<<\n")
+            while True:
+                print("O que deseja fazer?\n")
+                print("[1] SAQUE")
+                print("[2] SALDO")
+                print("[3] DEPÓSITO")
+                print("[4] EXTRATO")
+                print("[5] MENU CONTAS")
+                opcao = int(input("-- "))
                 print()
-                if self.saldo >= valor:
-                    print(f"Saque no valor de {valor} reais realizado.")
-                    saldo_atualizado = self.saldo - valor
-                    print(f"Saldo atual: {saldo_atualizado:.2f} reais.\n")
-                    print("OBRIGADO POR USAR NOSSOS SERVIÇOS!\nAté logo!")   
-                else:
-                    print("Saldo insuficiente.")
-                    print("Você pode ter limite de Cheque especial disponível.\nGostaria de consultar?")
-                    decisao_cheque_especial = int(input("[1] SIM\n[2] NÃO\n"))
-                    if decisao_cheque_especial == 1:
-                        self.salario = float(input("Para continuarmos, por favor, insira o valor do seu salário mensal: "))
-                        if self.salario >= 2000:
-                            self.cheque_especial = self.salario * 0.2
-                            print(f"Que legal! Você possui limite de Cheque especial no valor de {self.cheque_especial:.2f} reais.")
-                            opcao = int(input("Gostaria de contratar o Limite de Cheque Especial?\n[1] SIM\n[2] NÃO\n"))
-                            if opcao == 1:
-                                self.valor_total = self.saldo + self.cheque_especial
-                                print(f"Parabéns! Seu saldo para saque agora é de {self.valor_total:.2f} reais.\n")
-                                self.saldo = self.valor_total
-                                menu_inicial = int(input("Para ir para SAQUE, digite [1]: "))
-                                if menu_inicial == 1:
-                                    self.acao()
-                            elif opcao == 2:
-                                print("Você será redirecionado para o MENU INICIAL.")
-                                self.acao()
-                            else:
-                                menu_inicial = int(input("Para ir para SAQUE, digite [1]: "))
-                                if menu_inicial == 1:
-                                    self.acao()
-                        else:
-                            print("Infelizmente você não possui limite de Cheque especial.")
-                    elif decisao_cheque_especial == 2:
-                        menu_inicial = int(input("Você será redirecionado para o Menu inicial. Digite [2]: "))
-                        if menu_inicial == 2:
-                            self.acao()
-                        else:
-                            print("Opção inválida.")
-                    else:
-                        print("Por favor, selecione uma das opções")
-                        decisao_cheque_especial
-            elif opcao == 2:
-                print(f"Saldo {self.saldo:.2f} reais.\n")
-                print("Você está sendo redirecionado para as opções anteriores...\n")
-                self.acao()
-            elif opcao == 3:
-                valor_deposito = float(input("Informe o valor a ser depositado: "))
-                print()
-                self.saldo = valor_deposito + self.saldo
-                print(f"Valor de {valor_deposito:.2f} depositado com sucesso!\n")
-                print("Você está sendo redirecionado para as opções anteriores...\n")
-                self.acao()                     
-            else:
-                print("Opção inválida! \nPor favor, tente novamente.")
-                self.acao()           
-        elif self.conta_universitaria:
-            print("Informe uma opção:")
-            print("[1] Saque")
-            print("[2] Saldo")
-            opcao = int(input())
-            if opcao == 1:
-                valor = float(input("Informe a quantia para o saque: "))
-                if self.saldo >= valor:
-                    print(f"Saque no valor de {valor} reais realizado.")
-                    saldo_atualizado = self.saldo - valor
-                    print(f"Saldo atualizado: {saldo_atualizado:.2f} reais.\n")
-                    print("OBRIGADO POR USAR NOSSOS SERVIÇOS!\nAté logo!")
-                else:
-                    print("Saldo insuficiente.")
-            elif opcao == 2:
-                print(f"Saldo {self.saldo:.2f} reais.")
-                opcao = int(input("Para retornar ao SAQUE, digite o [1]: \n"))
                 if opcao == 1:
-                    self.acao()
-            elif opcao == 3:
-                valor_deposito = float(input("Informe o valor a ser depositado: \n"))
-                self.saldo = valor_deposito + self.saldo
-                print(f"Valor de {valor_deposito:.2f} depositado com sucesso!\n")
-                print("Retornando as opções anteriores.\n")
-                self.acao()
-            else:
-                print("Opção inválida! \nPor favor, tente novamente.")
-                self.conta_universitaria
+                    self.saque()
+                elif opcao == 2:
+                    self.consultar_saldo()
+                elif opcao == 3:
+                    self.deposito()
+                elif opcao == 4:
+                    self.exibir_extrato()
+                elif opcao == 5:
+                    return
+                else:
+                    print("Opção inválida!\nPor favor, tente novamente.\n")
+        elif self.conta_universitaria:
+            print("\n>>>> CONTA UNIVERSITÁRIA <<<<\n")
+            while True:
+                print("O que deseja fazer:\n")
+                print("[1] SAQUE")
+                print("[2] SALDO")
+                print("[3] DEPÓSITO")
+                print("[4] MENU CONTAS")
+                opcao = int(input("-- "))
+                print()
+                if opcao == 1:
+                    self.saque()
+                elif opcao == 2:
+                    self.consultar_saldo()
+                elif opcao == 3:
+                    self.deposito()
+                elif opcao == 4:
+                    return
+                else:
+                    print("Opção inválida!\nPor favor, tente novamente.\n")
+
+    def saque(self):
+        print(">>>>>>>>>>>> SAQUE <<<<<<<<<<<<\n")
+        valor = float(input("Informe a quantia para o saque: "))
+        print()
+        if self.saldo >= valor:
+            print(f"Saque no valor de R$ {valor} realizado.")
+            self.saldo -= valor
+            self.extrato += f"Saque: R$ {valor:.2f}\n"
+            print(f"Saldo atual: {self.saldo:.2f}.\n")
+            print("Voltando ao MENU anterior...\n")
+        else:
+            print("Saldo insuficiente.\n")
+            self.cheque_especial()
+
+    def consultar_saldo(self):
+        print(">>>>>>>>>>>> SALDO <<<<<<<<<<<<\n")
+        print(f"Saldo R$ {self.saldo:.2f}\n")
+        print(">>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\n")
+        opcao = int(input("[1] MENU\n-- "))
+        if opcao == 1:
+           self.acao()
+        else:
+            print("\nOpção inválida!\n")
+            return False
+            
+    def deposito(self):
+        print(">>>>>>>>>>>> DEPÓSITO <<<<<<<<<<<<\n")
+        valor_deposito = float(input("Informe o valor a ser depositado: "))
+        print()
+        self.saldo += valor_deposito
+        self.extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
+        print(f"Valor de R$ {valor_deposito:.2f} depositado com sucesso!\n")
+
+    def cheque_especial(self):
+        print("Você pode ter limite de Cheque especial disponível.\nGostaria de consultar?\n")
+        decisao_cheque_especial = int(input("[1] SIM\n[2] NÃO\n-- "))
+        if decisao_cheque_especial == 1:
+            print()
+            print(">>>>>>>>>>>> CHEQUE ESPECIAL <<<<<<<<<<<<\n")
+            self.salario = float(input("Para continuarmos, por favor, insira o valor do seu salário mensal: "))
+            if self.salario >= 2000:
+                self.cheque_especial = self.salario * 0.2
+                print()
+                print(f"Que legal! Você possui limite de Cheque especial no valor de R$ {self.cheque_especial:.2f}.")
+                opcao = int(input("Gostaria de contratar o Limite de Cheque Especial?\n\n[1] SIM\n[2] NÃO\n-- "))
+                if opcao == 1:
+                    self.valor_total = self.saldo + self.cheque_especial
+                    print()
+                    print(f"Parabéns! Seu saldo para saque agora é de R${self.valor_total:.2f}.\n")
+                    self.saldo = self.valor_total
+                    self.extrato += f"Cheque especial: R$ {self.cheque_especial:.2f}\n"            
+
+    def exibir_extrato(self):
+        print("================ EXTRATO ================\n")
+        print("Não foram realizadas movimentações." if not self.extrato else self.extrato)
+        print(f"\nSaldo: R$ {self.saldo:.2f}")
+        print("=========================================\n")
 
 conta_corrente = True
 conta_universitaria = False
-           
+
 b = Banco(conta_corrente, conta_universitaria)
-if b.autentificacao():
-    if b.contas():
+if b.autenticacao():
+    while True:
+        if not b.selecionar_conta():
+            break
         b.acao()
